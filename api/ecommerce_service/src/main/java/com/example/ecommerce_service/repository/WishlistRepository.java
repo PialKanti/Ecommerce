c0a20027a5669db2959e4912028810c46922c3bc
Pial Kanti Samadder <pialkanti2012@gmail.com>
2025-07-25 16:07:43 +0600

    Fixed customer wishlist mapping issue

--- api/ecommerce_service/src/main/java/com/example/ecommerce_service/repository/WishlistRepository.java
+++ api/ecommerce_service/src/main/java/com/example/ecommerce_service/repository/WishlistRepository.java
@@ -6,12 +6,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
     @Query("""
-            select product.id,
-                   product.name,
-                   product.description
+            select product.id as id,
+                   product.name as name,
+                   product.description as description
             from Wishlist wishlist
             join wishlist.product product
             where wishlist.customer.id = :customerId
