8b777ffd0b61f1093531aa1466336c1c70a68dd0
Pial Kanti Samadder <pialkanti2012@gmail.com>
2025-07-25 16:43:04 +0600

    Thrown exception if customer id is not valid

--- api/ecommerce_service/src/main/java/com/example/ecommerce_service/service/impl/CustomerServiceImpl.java
+++ api/ecommerce_service/src/main/java/com/example/ecommerce_service/service/impl/CustomerServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.ecommerce_service.service.impl;
 
 import com.example.ecommerce_service.dto.response.PaginatedApiResponse;
+import com.example.ecommerce_service.enums.ExceptionConstant;
 import com.example.ecommerce_service.projection.WishlistItemProjection;
+import com.example.ecommerce_service.repository.CustomerRepository;
 import com.example.ecommerce_service.repository.WishlistRepository;
 import com.example.ecommerce_service.service.CustomerService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,10 +17,15 @@ import static com.example.ecommerce_service.constant.MessageConstants.WISHLIST_I
 @Service
 @RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
     private final WishlistRepository wishlistRepository;
 
     @Override
     public PaginatedApiResponse<WishlistItemProjection> findWishlistItemsByCustomerId(Long customerId, Pageable pageable) {
+        if (!customerRepository.existsById(customerId)) {
+            throw new EntityNotFoundException(ExceptionConstant.CUSTOMER_NOT_FOUND.getMessage());
+        }
+
         Page<WishlistItemProjection> pageResponse = wishlistRepository.findWishlistItemsByCustomerId(customerId, pageable);
 
         return PaginatedApiResponse.success(WISHLIST_ITEMS_RETRIEVED, pageResponse);
